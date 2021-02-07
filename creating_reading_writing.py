ebfa5d34e1a528c43cb7fdb034f4bfefe9de3627
bthakr1 <b.thakr@mail.analytics.hbs.edu>
2021-02-07 12:04:55 -0600

    added some code

--- creating_reading_writing.py
+++ creating_reading_writing.py
@@ -16,11 +16,11 @@ new_matrix = pd.DataFrame({'Bob':['I Liked it','It was aweful'],'Sue':['Pretty G
 print(new_matrix)
 
 # Series
-
+# Simple series
 new_series = pd.Series([1,2,3,4,5])
-
+# Printing the simple series
 print(new_series)
-
+# Creating a year series. This is where Series are really powerful with indexes
 year_sales = pd.Series([30,45,50], index=['Sales_2018','Sales_2019','Sales_2020'], name='Product_A')
-
+# Printing the year_sales
 print(year_sales)
