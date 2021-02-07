926bd7462619946c3922b3c5c89c003140073cb0
bthakr1 <b.thakr@mail.analytics.hbs.edu>
2021-02-07 11:41:09 -0600

    Added new branch and also created a new matrix

--- creating_reading_writing.py
+++ creating_reading_writing.py
@@ -1,5 +1,16 @@
+# importing pandas 
 import pandas as pd
 
-classification_matrix = pd.DataFrame({'Yes':[50,21],'No':[131,21]})
+# creating a matrix with yes and no
+classification_matrix = pd.DataFrame({'Yes':[50,21],'No':[131,21]},index=['Yes','No'])
 
+# Printing the matrix
 print(classification_matrix)
+
+# Let's create a new repository
+
+new_matrix = pd.DataFrame({'Bob':['I Liked it','It was aweful'],'Sue':['Pretty Good','Bad']}, index=['Comment 1','Comment 2'])
+
+# print the new matrix
+
+print(new_matrix)
