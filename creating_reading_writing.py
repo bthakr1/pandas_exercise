b8a779f97974571c112e5c9e00f6021428687682
bthakr1 <b.thakr@mail.analytics.hbs.edu>
2021-02-07 11:52:34 -0600

    Added series and some new things to it

--- creating_reading_writing.py
+++ creating_reading_writing.py
@@ -14,3 +14,13 @@ new_matrix = pd.DataFrame({'Bob':['I Liked it','It was aweful'],'Sue':['Pretty G
 # print the new matrix
 
 print(new_matrix)
+
+# Series
+
+new_series = pd.Series([1,2,3,4,5])
+
+print(new_series)
+
+year_sales = pd.Series([30,45,50], index=['Sales_2018','Sales_2019','Sales_2020'], name='Product_A')
+
+print(year_sales)
